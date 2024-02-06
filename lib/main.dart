230637bd9a7fb35e17ef9cb486eaa03a03a9eb60
Goviprasath19 <155996577+Goviprasath19@users.noreply.github.com>
2024-02-06 18:25:56 +0530

    Add files via upload
    Signed-off-by: Goviprasath19 <155996577+Goviprasath19@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,78 @@
-import 'dart:io' show Platform;
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:sandhut/app/pages/expense/expense_enter.dart';
-import 'package:sandhut/expense_file/expense_page.dart';
-import 'package:sandhut/widgets/pdfVoucherDesign.dart';
-import 'package:sandhut/widgets/splashScreen.dart';
+import 'file_handle_api.dart';
+import 'pdf_invoice_api.dart';
 
-import 'app/authentication/login.dart';
-import 'app/pages/dashboard/admin_dashboard.dart';
-import 'app/pages/tax_invoice/tax_invoice_generator/tax_invoice_pdf.dart';
-import 'app/pages/voucher/VoucherEnter.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: FirebaseOptions(
-    apiKey: "AIzaSyDxxNq-33-dYAGO0rl9nFPhslOYKh_xHoE",
-    authDomain: "sandhut-7353.firebaseapp.com",
-    projectId: "sandhut-7353",
-    messagingSenderId: "277386462714",
-    appId: "1:277386462714:web:31af2a9dd04e8e33769043",
-    storageBucket: "sandhut-7353.appspot.com",
-  ));
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SandHut ERP',
-      // home: const Side_Menu_Bar(),
-      home: ExpenseEnter(),
-      // home: const Login(),
-      // home: const PdfVoucherDesign(),
+    return MaterialApp(
+      title: 'Invoice PDF Generate',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Invoice'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.picture_as_pdf,
+              size: 72.0,
+              color: Colors.white,
+            ),
+            const SizedBox(height: 15.0),
+            const Text(
+              'Generate Invoice',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            ElevatedButton(
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 8.0),
+                child: Text(
+                  'Invoice PDF',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              onPressed: () async {
+                // generate pdf file
+                final pdfFile = await PdfInvoiceApi.generate();
+
+                // opening the pdf file
+                FileHandleApi.openFile(pdfFile);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
